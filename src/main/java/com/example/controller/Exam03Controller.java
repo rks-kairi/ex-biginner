b28ba18cb6f6bac433739e37ec5cbd2e2e3cf2cf
hashimotokairi <kairi.hashimoto@rakus.co.jp>
2021-05-20 15:56:17 +0900

    入力した商品の価格を表示させる機能が完成しました。
    演習3 exam03.htmlを作成しました
    
    演習2 Exam03Controllerを作成しました
    
    演習3 exam03-result.htmlを作成しました

---
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("exam03")
+public class Exam03Controller {
+	@Autowired
+	private ServletContext application;
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam03";
+	}
+	
+	@RequestMapping("/total")
+	public String total(Integer item1, Integer item2, Integer item3) {
+		application.setAttribute("item1", item1);
+		application.setAttribute("item2", item2);
+		application.setAttribute("item3", item3);
+		application.setAttribute("total", item1 + item2 + item3);
+		application.setAttribute("tax", (item1 + item2 + item3) * 1.1);
+		return "exam03-result";
+	}
+}
